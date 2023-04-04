29782390d763f738156f59323b89560ffca7a2c7
amahadaya <118939810+amahadaya@users.noreply.github.com>
2023-04-04 17:32:52 -0400

    Resolve VCSWP-19101 (#57)
    Add signing secret verification util classes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,14 @@ This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 
 None
 
+<a name="3.2.0"></a>
+
+## [3.2.0] - 2023-04-03
+
+### Added
+
+- Introduce signing secret verification class (RequestVerifier) - https://docs.freeclimb.com/docs/validating-requests-from-freeclimb#how-to-verify-requests-manually
+
 <a name="3.1.3"></a>
 
 ## [3.1.3] - 2023-03-13
---
+++ utils/SignatureInformation.ts
@@ -0,0 +1,39 @@
+import * as moment from 'moment';
+import * as crypto from 'crypto';
+
+export class SignatureInformation {
+    requestTimestamp: number = 0;
+    signatures: Array<string> = [];
+
+    constructor(requestHeader: string) {
+        const signatureHeaders: Array<string> = requestHeader.split(",")
+        signatureHeaders.forEach((signature) => {
+            const [header, value] = signature.split("=")
+            if (header === "t") {
+                this.requestTimestamp = parseInt(value)
+            }
+            else if (header === "v1") {
+                this.signatures.push(value)
+            }
+        })
+    }
+
+    isRequestTimeValid(tolerance: number):boolean {
+        var currentUnixTimestamp = this.getCurrentUnixTime();
+        return (this.requestTimestamp + tolerance) < currentUnixTimestamp;
+    }
+
+    isSignatureSafe(requestBody: string, signingSecret: string ): boolean{
+        const hashValue: string = this.computeHash(requestBody, signingSecret);
+        return this.signatures.includes(hashValue);
+    }
+
+    private computeHash(requestBody: string, signingSecret: string): string {
+        const data: string = this.requestTimestamp + '.' + requestBody;
+        return crypto.createHmac('sha256', signingSecret).update(data).digest('hex');
+    }
+
+    getCurrentUnixTime(): number {
+        return moment().unix();
+    }
+}
